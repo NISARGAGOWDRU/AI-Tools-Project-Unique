ef1dfe4ec8a978915860a588a979232d4ce1a443
Nisarga M K <nisarga@techmust.com>
2025-10-28 11:46:33 +0530

    port rectified

--- new-backend/app/main.py
+++ new-backend/app/main.py
@@ -143,6 +143,6 @@ async def run_pipeline(payload: RunPipelinePayload):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True, log_level="info")
+    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True, log_level="info")
 
 
